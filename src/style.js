e39e7e9fb135346644fb82fc634213a3599f77db
biharck <biharck@gmail.com>
2019-04-04 09:52:06 -0300

    adding text default options

--- src/style.js
+++ src/style.js
@@ -1,25 +1,37 @@
-import {Fill, Stroke, Style as OpenLayersStyle, Circle as CircleStyle} from 'ol/style.js';
+import {Fill, Stroke, Text, Style as OpenLayersStyle, Circle as CircleStyle} from 'ol/style.js';
 
 const _style = Symbol('style')
 
 class ToolStyle {  
   
-    constructor(strokeOptions, fillOptions, circleOptions, textOptions) {
+    constructor(styleOptions) {
 
       const style = new OpenLayersStyle({
         stroke: new Stroke({
-          color: strokeOptions.color,
-          width: strokeOptions.width
+          color: styleOptions.stroke.color !== undefined ? styleOptions.stroke.color : 'green',
+          width: styleOptions.stroke.width !== undefined ? styleOptions.stroke.width : 1
         }),
         fill: new Fill({
-          color: fillOptions.color
+          color: styleOptions.fill.color !== undefined ? styleOptions.fill.color : 'rgba(0, 255, 0, 0.1)'
         }),
         image: new CircleStyle({
-          radius: circleOptions.radius,
+          radius: styleOptions.circle.radius !== undefined ? styleOptions.circle.radius : 5,
           fill: new Fill({
-            color: circleOptions.color
+            color: styleOptions.circle.color !== undefined ? styleOptions.circle.color : 'green'
           })
-        })
+        }),
+        text : styleOptions.text !== undefined ? new Text({
+          font: '10px sans-serif',
+          textAlign : 'center',  
+          text : 'teste',
+          offsetX : 0,
+          offsetY : 0,
+          scale : 1,
+          textBaseline : 'center',
+          fill : new Fill({
+            color: '#333'
+          })
+        }) : new Text()
       })
 
       this[_style] = style
