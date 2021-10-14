684c0ab7f999807be8df6b5f225922a5221b7da0
hackermd <hackermd@protonmail.com>
2021-10-14 08:42:34 -0400

    Only warn if slide coordinates are negative

--- src/channel.js
+++ src/channel.js
@@ -629,8 +629,8 @@ class _Channel {
       ...new Set(physicalSizes.map(v => v.toString()))
     ].map(v => v.split(','))
     if (uniquePhysicalSizes.length > 1) {
-      throw new Error(
-        'Images of the image pyramid have different sizes (in millimeter): ',
+      console.warn(
+        'images of the image pyramid have different sizes (in millimeter): ',
         physicalSizes
       )
     }
