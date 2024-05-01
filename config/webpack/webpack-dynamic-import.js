ddff79f898cd873b57e23417d07715ce236b6ce9
Igor Octaviano <igoroctaviano@gmail.com>
2024-05-01 08:16:51 -0300

    Revert "fix: Setup the dynamic import so it is separate from other packages (#113)"
    This reverts commit 28349445228ab2f4b67504c0f06f21710ebe495a.

--- config/webpack/webpack-dynamic-import.js
+++ config/webpack/webpack-dynamic-import.js
@@ -3,7 +3,7 @@ const merge = require('./merge')
 const rootPath = process.cwd()
 const baseConfig = require('./webpack-base')
 const TerserPlugin = require('terser-webpack-plugin')
-const outputPath = path.join(rootPath, 'dist', 'dynamic-import', 'dicom-microscopy-viewer')
+const outputPath = path.join(rootPath, 'dist', 'dynamic-import')
 
 const prodConfig = {
   mode: 'production',
@@ -12,15 +12,11 @@ const prodConfig = {
   },
   output: {
     path: outputPath,
-    library: {
-      name: 'dicomMicroscopyViewer',
-      type: 'window',
-    },
-    filename: '[name].min.js',
-    publicPath: '/dicom-microscopy-viewer/',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    filename: '[name].min.js'
   },
   optimization: {
-    // minimize: false,
     minimizer: [
       new TerserPlugin({
         parallel: true
