f546391fc756f2512d62f4f6f3dd9ccfb56b3885
sedghi <ar.sedghi@gmail.com>
2025-04-01 13:46:00 -0400

    fix

--- bun.build.js
+++
@@ -1,9 +0,0 @@
-await Bun.build({
-  entrypoints: ["./src/dicom-microscopy-viewer.js"],
-  outdir: "./dist/esm",
-  splitting: true,
-  sourcemap: "none",
-  format: "esm",
-  target: "browser",
-  minify: true,
-})
