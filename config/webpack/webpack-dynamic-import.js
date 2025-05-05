11d3ff1ff05ee6585d1653cbc01cd587b9b84bab
Igor Octaviano <igoroctaviano@gmail.com>
2025-05-05 18:53:05 -0300

    Apply: Setup the dynamic import so it is separate from other packages by wayfarer3130

--- config/webpack/webpack-dynamic-import.js
+++ config/webpack/webpack-dynamic-import.js
@@ -3,7 +3,7 @@ const merge = require('./merge')
 const rootPath = process.cwd()
 const baseConfig = require('./webpack-base')
 const TerserPlugin = require('terser-webpack-plugin')
-const outputPath = path.join(rootPath, 'dist', 'dynamic-import')
+const outputPath = path.join(rootPath, 'dist', 'dynamic-import', 'dicom-microscopy-viewer')
 
 const prodConfig = {
   mode: 'production',
@@ -12,9 +12,12 @@ const prodConfig = {
   },
   output: {
     path: outputPath,
-    libraryTarget: 'umd',
-    globalObject: 'window',
-    filename: '[name].min.js'
+    library: {
+      name: 'dicomMicroscopyViewer',
+      type: 'window',
+    },
+    filename: '[name].min.js',
+    publicPath: '/dicom-microscopy-viewer/',
   },
   optimization: {
     minimizer: [
