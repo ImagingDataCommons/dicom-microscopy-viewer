d0f2aa84775d7903cdd333acebe936338e048a52
Bill Wallace <wayfarer3130@gmail.com>
2025-07-28 15:05:42 -0400

    Try to fix build/test

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,22 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      targets: {
-        browsers: ['ie 11']
-      }
-    }]
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          browsers: ["ie 11"],
+        },
+      },
+    ],
   ],
   plugins: [
-    ['@babel/plugin-transform-runtime',
+    "babel-plugin-transform-import-meta",
+    [
+      "@babel/plugin-transform-runtime",
       {
         regenerator: true,
-        corejs: 3
-      }
-    ]
-  ]
-}
+        corejs: 3,
+      },
+    ],
+  ],
+};
